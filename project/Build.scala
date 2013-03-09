ee8e8423cd78b76f4d20604a254b3f77e91d69fc
toshiyuki <t.toshi.0412@gmail.com>
2013-03-09 23:31:54 +0900

    Conscripted

---
+++ project/Build.scala
@@ -0,0 +1,24 @@
+import sbt._
+import Keys._
+
+object AAProject extends Build {
+
+  lazy val root = Project (
+    id = "scala-ascii-art",
+    base = file ("."),
+    settings = Defaults.defaultSettings ++ Seq (
+      organization := "com.github.cb372",
+      name:= "scala-ascii-art",
+      scalaVersion := "2.10.0",
+      scalacOptions ++= Seq("-deprecation"),
+      libraryDependencies ++= Seq(
+        "net.databinder.dispatch" %% "dispatch-core" % "0.9.5",
+        "org.slf4j" % "slf4j-nop" % "1.6.2",
+        "com.github.cb372" %% "rainbow" % "0.2",
+        "org.scala-sbt" % "command" % "0.12.2"
+      ),
+      resolvers += "Chris Birchall's Maven repo" at "http://cb372.github.com/m2/releases"
+    )
+  )
+}
+
--- project/plugins.sbt
+++ project/plugins.sbt
@@ -1,1 +1,3 @@
 addSbtPlugin("com.github.retronym" % "sbt-onejar" % "0.8")
+
+addSbtPlugin("net.databinder" % "conscript-plugin" % "0.3.5")
