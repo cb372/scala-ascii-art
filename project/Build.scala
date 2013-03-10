3391d6015fe2106c8f9069b312140657b657a6e5
Chris Birchall <chris.birchall@gmail.com>
2013-03-10 09:58:37 +0900

    Set version

--- project/Build.scala
+++ project/Build.scala
@@ -8,7 +8,8 @@ object AAProject extends Build {
     base = file ("."),
     settings = Defaults.defaultSettings ++ Seq (
       organization := "com.github.cb372",
-      name:= "scala-ascii-art",
+      name := "scala-ascii-art",
+      version := "0.1",
       scalaVersion := "2.10.0",
       scalacOptions ++= Seq("-deprecation"),
       libraryDependencies ++= Seq(
@@ -17,7 +18,8 @@ object AAProject extends Build {
         "com.github.cb372" %% "rainbow" % "0.2",
         "org.scala-sbt" % "command" % "0.12.2"
       ),
-      resolvers += "Chris Birchall's Maven repo" at "http://cb372.github.com/m2/releases"
+      resolvers += "Chris Birchall's Maven repo" at "http://cb372.github.com/m2/releases",
+      publishTo := Some(Resolver.file("file",  new File( "../cb372.github.com/m2/releases" )))
     )
   )
 }
